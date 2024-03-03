3a6c2d0dd243fff457b41dc293839732f61388a5
lilyyang0 <158564820+lilyyang0@users.noreply.github.com>
2024-03-03 09:48:34 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.c
+++ 프로그래머스/0/120829. 각도기/각도기.c
@@ -3,5 +3,5 @@
 #include <stdlib.h>
 
 int solution(int angle) {
-    return angle>0&&angle<90? 1: angle==90?2: angle>90&&angle<180? 3: angle ==180?4:0;
+    return angle>0&&angle<90? 1: angle==90?2: angle>90&&angle<180? 3: 4;
 }
